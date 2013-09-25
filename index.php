4907b0cc52b8ae2166d06c3ef38b59864d3d4c0b
Nguyen Hoang Trung <hoangtrung254@live.com>
2013-09-25 19:35:51 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Just a test</title>
+<link rel="stylesheet" type="text/css" href="bootstrap.css">
+</head>
+<body>
+<ul class="nav nav-tabs">
+<li> <a href="home">Home</a> </li>
+<li> <a href="about">About</a> </li>
+<li> <a href="contact">Contact</a> </li>
+</ul>
+</body>
+</html>
